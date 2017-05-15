f16635783b19732219f0b2395e9ad52d3ba726cf
Paul Murray <awesomeness872@users.noreply.github.com>
2017-05-14 22:12:04 -0500

    fixed 'expected first header'

--- Source/QtCreatorSourceCodeAccess/Private/QtCreatorSourceCodeAccessModule.cpp
+++ Source/QtCreatorSourceCodeAccess/Private/QtCreatorSourceCodeAccessModule.cpp
@@ -20,9 +20,9 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#include "QtCreatorSourceCodeAccessModule.h"
 #include "QtCreatorSourceCodeAccessPrivatePCH.h"
 #include "Runtime/Core/Public/Features/IModularFeatures.h"
-#include "QtCreatorSourceCodeAccessModule.h"
 
 IMPLEMENT_MODULE( FQtCreatorSourceCodeAccessModule, QtCreatorSourceCodeAccess );
 
